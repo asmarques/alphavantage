f8c1485d4c3563b679c7f973e2ff8810f4e17ad5
António Marques <me@antoniomarques.eu>
2018-12-06 20:30:18 +0000

    Remove 'extern crate' declarations which are no longer needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-extern crate chrono;
-extern crate chrono_tz;
-extern crate failure;
-extern crate failure_derive;
-extern crate reqwest;
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_json;
-
 mod client;
 mod deserialize;
 
