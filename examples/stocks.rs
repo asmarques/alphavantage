96a3f3f78af52c0379eda2c7c87d4efb4576d9a9
António Marques <me@antoniomarques.eu>
2018-12-09 17:04:08 +0000

    Add 'stocks' example application

---
+++ examples/stocks.rs
@@ -0,0 +1,59 @@
+use alphavantage::time_series::IntradayInterval;
+use alphavantage::Client;
+use exitfailure::ExitFailure;
+use failure::{err_msg, format_err};
+use std::env;
+use structopt::StructOpt;
+
+const TOKEN_ENV_KEY: &str = "ALPHAVANTAGE_TOKEN";
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(
+        short = "t",
+        long = "token",
+        help = "API token (can use the ALPHAVANTAGE_TOKEN env var instead)"
+    )]
+    token: Option<String>,
+    #[structopt(help = "period (1min, 5min, 15min, 30min, hourly, daily, weekly or monthly)")]
+    period: String,
+    #[structopt(help = "stock symbol (e.g. AAPL)")]
+    symbol: String,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let args = Cli::from_args();
+    let token = args
+        .token
+        .or_else(|| env::var(TOKEN_ENV_KEY).ok())
+        .ok_or_else(|| err_msg("missing token"))?;
+
+    let symbol = &args.symbol;
+    let client = Client::new(&token);
+
+    let time_series = match args.period.as_str() {
+        "1min" => client.get_time_series_intraday(symbol, IntradayInterval::OneMinute),
+        "5min" => client.get_time_series_intraday(symbol, IntradayInterval::FiveMinutes),
+        "15min" => client.get_time_series_intraday(symbol, IntradayInterval::FifteenMinutes),
+        "30min" => client.get_time_series_intraday(symbol, IntradayInterval::ThirtyMinutes),
+        "hourly" => client.get_time_series_intraday(symbol, IntradayInterval::SixtyMinutes),
+        "daily" => client.get_time_series_daily(symbol),
+        "weekly" => client.get_time_series_weekly(symbol),
+        "monthly" => client.get_time_series_monthly(symbol),
+        _ => Err(format_err!("unknown period {}", args.period))?,
+    }?;
+
+    println!(
+        "{} (updated: {})\n",
+        time_series.symbol, time_series.last_refreshed
+    );
+    for entry in time_series.entries {
+        println!("{}:", entry.date);
+        println!("  open: {}", entry.open);
+        println!("  high: {}", entry.high);
+        println!("  low: {}", entry.low);
+        println!("  close: {}", entry.close);
+        println!("  volume: {}", entry.volume);
+    }
+    Ok(())
+}
